3fd94cdd452f7815fd36800fa65457590902f01c
david111671 <davidchaikovsky100@gmail.com>
2024-04-13 15:42:01 +0300

    Removes all .gitignore files and folders

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'Skinet';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5206/api/products').subscribe((response: any) => {
+      console.log(response)
+    })
+  }
+}
